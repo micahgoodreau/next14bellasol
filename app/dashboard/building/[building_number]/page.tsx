190924c961eb22b074433ffc0fde22f307f8866f
micahgoodreau0912 <micahgoodreau@gmail.com>
2023-11-07 00:46:55 -0500

    update bellasol management system to next14

--- app/dashboard/building/[building_number]/page.tsx
+++ app/dashboard/building/[building_number]/page.tsx
@@ -3,11 +3,12 @@ import { cookies } from "next/headers";
 
 import type { Database } from "@/database.types";
 import PropertyDetail from "@/app/ui/dashboard/PropertyDetail";
+import Link from "next/link";
 
 export default async function Page({
   params: { building_number },
 }: {
-  params: { building_number: string };
+  params: { building_number: number };
 }) {
   const cookieStore = cookies();
   const supabase = createServerClient<Database>(
@@ -28,12 +29,15 @@ export default async function Page({
     }
   );
 
+  const prev_building_number: number = building_number - 1;
+  const next_building_number: number = Number(building_number) + Number(1);
+
   const { data: properties } = await supabase
     .from("properties")
     .select(
       `id, unit_number, building_number, leepa_owners(owner_name, address1, address2, address3, address4, country)`
     )
-    .match({ building_number: building_number })
+    .match({ building_number: building_number.toString() })
     .order("unit_number", { ascending: true });
 
   if (properties === null || properties.length === 0)
@@ -43,13 +47,66 @@ export default async function Page({
     <>
       <div className="grid grid-cols-2 gap-2 h-screen p-2">
         <div className="">
+          <div className="grid grid-cols-2 gap-2 h-20 p-2">
+            <div>
+              <Link
+                href={`/dashboard/building/${prev_building_number}`}
+                className="flex w-44 items-center rounded-md bg-btn-background px-4 py-2 text-sm text-foreground no-underline hover:bg-btn-background-hover"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+                >
+                  <polyline points="15 18 9 12 15 6" />
+                </svg>{" "}
+                Previous Building
+              </Link>
+            </div>
+            <div>
+              <Link
+                href={`/dashboard/building/${next_building_number}`}
+                className="w-44 flex items-center rounded-md bg-btn-background ml-2 px-4 py-2 text-sm text-foreground no-underline hover:bg-btn-background-hover"
+              >
+                Next Building
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4 transition-transform group-hover:translate-x-1"
+                >
+                  <polyline points="15 18 22 12 15 6" />
+                </svg>{" "}
+              </Link>
+            </div>
+          </div>
+
           {properties.map((property) => (
             <p key={property.id} className="py-2 border-b">
-              {property.unit_number} {property.leepa_owners?.owner_name}{" "}
+              <Link
+                href={`/dashboard/building/${building_number}/${property.id}`}
+              >
+                {property.unit_number} {property.leepa_owners?.owner_name}{" "}
+              </Link>
             </p>
           ))}
         </div>
-        <PropertyDetail property_id="0d0362b7-3b5d-4229-bd64-cbafd5288245" />
+        <div>
+          <p>Select property from list to show details.</p>
+        </div>
       </div>
     </>
   );
